04b6c6235ef85dd49dae89e3b7ba26958506a00f
vamsiKrishna-511 <devulapallivamsi197@gmail.com>
2022-12-28 12:04:49 +0000

    congrats card

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  <div class="card-container">
+    <h1 class="heading">Congratulations</h1>
+    <div class="container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        class="img"
+      />
+      <h1 class="title">Kiran V</h1>
+      <p class="description">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        class="img2"
+      />
+    </div>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
